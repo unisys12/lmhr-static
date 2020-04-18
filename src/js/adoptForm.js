9b919cd654df953c7fd06ae6e99fbdab961c4269
unisys12 <unisys12@gmail.com>
2020-04-18 07:46:01 -0500

    adjust app and control script

--- src/js/adoptForm.js
+++ src/js/adoptForm.js
@@ -4,44 +4,50 @@ const workElement = document.querySelector("#employment_status");
 const workInput = document.querySelectorAll(".work_option");
 const landLordSelector = document.querySelector("#own_or_rent");
 const landlord = document.querySelectorAll(".landlord");
-const vetSelector = document.querySelector("#veterinarain");
+const vetSelector = document.querySelector("#veterinarian");
 const vetInputs = document.querySelectorAll(".vet_details");
 
 vetSelector.addEventListener("change", e => {
-  let vet_assoc = e.target.value === "2";
+  let vet_assoc = e.target.value;
 
-  vetInputs.forEach(x => {
-    if (vet_assoc && !x.hasAttribute("disabled", "")) {
-      x.setAttribute("disabled", "");
-    }
-    if (!vet_assoc && x.hasAttribute("disabled", "")) {
-      x.removeAttribute("disabled");
-    }
-  });
+  console.log(`Fire change event for Vet Selector`);
+
+  if (vet_assoc == "1") {
+    vetInputs.forEach(v => v.removeAttribute('disabled'));
+  } else {
+    vetInputs.forEach(v => v.setAttribute('disabled', ''));
+  }
 });
 
 workElement.addEventListener("change", e => {
-  let retired = e.target.value === "retired";
+  let workStatus = e.target.value;
 
-  workInput.forEach(x => {
-    if (retired && !x.hasAttribute("disabled", "")) {
-      x.setAttribute("disabled", "");
-    }
-    if (!retired && x.hasAttribute("disabled", "")) {
-      x.removeAttribute("disabled");
-    }
-  });
+  switch (workStatus) {
+    case "1":
+      workInput.forEach(w => w.removeAttribute('disabled'));
+      break;
+
+    case "2":
+      workInput.forEach(w => w.removeAttribute('disabled'));
+      break;
+
+    case "3":
+      workInput.forEach(w => w.setAttribute('disabled', ''));
+      break;
+
+    case "4":
+      workInput.forEach(w => w.setAttribute('disabled', ''));
+      break;
+  
+    default:
+      break;
+  }
 });
 
 landLordSelector.addEventListener("change", e => {
-  let own = e.target.value === "1";
+  let own = e.target.value;
 
-  landlord.forEach(ele => {
-    if (own && !ele.hasAttribute("disabled")) {
-      ele.setAttribute("disabled", "");
-    }
-    if (!own && ele.hasAttribute("disabled")) {
-      ele.removeAttribute("disabled");
-    }
-  });
+  if (own == "2") {
+    landlord.forEach(l => l.removeAttribute('disabled'));
+  } 
 });
